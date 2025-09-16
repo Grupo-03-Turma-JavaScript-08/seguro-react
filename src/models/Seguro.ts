2d01f4160a239042ba9f408d937be079c6ddc259
Maeli <maeli.palharini@hotmail.com>
2025-09-16 15:03:41 -0300

    ✨ feat: reativar interface Seguro com importações necessárias

--- src/models/Seguro.ts
+++ src/models/Seguro.ts
@@ -1,14 +1,14 @@
-// import { Categoria } from "./Categoria";
-// import { Usuario } from "./Usuario";
-//
-// export interface Seguro {
-//     id?: number;
-//     nome: string;
-//     descricao: string;
-//     origem: string;
-//     destino: string;
-//     duracaoDias: number;
-//     preco: number;
-//     categoria: Categoria;
-//     usuario?: Usuario | null;
-// }
+import { Categoria } from "./Categoria";
+import { Usuario } from "./Usuario";
+
+export interface Seguro {
+    id?: number;
+    nome: string;
+    descricao: string;
+    origem: string;
+    destino: string;
+    duracaoDias: number;
+    preco: number;
+    categoria: Categoria;
+    usuario?: Usuario | null;
+}
